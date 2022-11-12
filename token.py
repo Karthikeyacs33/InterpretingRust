c0ebee27360794eeb26bfdb45eb0b38cac188163
Karthikeyacs33 <113897742+Karthikeyacs33@users.noreply.github.com>
2022-11-12 19:58:48 +0530

    hehe

--- token.py
+++ token.py
@@ -1,3 +1,28 @@
+from enum import Enum
+
+class ErrorCode(Enum):
+    UNEXPECTED_TOKEN = 'Unexpected token'
+    ID_NOT_FOUND     = 'Identifier not found'
+    DUPLICATE_ID     = 'Duplicate id found'
+
+class Error(Exception):
+    def __init__(self, error_code=None, token=None, message=None):
+        self.error_code = error_code
+        self.token = token
+        # add exception class name before the message
+        self.message = f'{self.__class__.__name__}: {message}'
+
+class LexerError(Error):
+    pass
+
+class ParserError(Error):
+    pass
+
+class SemanticError(Error):
+    pass
+
+
+
 INTEGER = 'INTEGER'
 NUMBER = 'NUMBER'
 TRUE = 'true'
@@ -33,24 +58,30 @@ ELSEIF = 'else if'
 ELSE = 'else'
 LET = 'let'
 WHILE = 'while'
+PRINT = "println!"
 
 EOF = 'EOF'
 
 class Token(object):
-    def __init__(self, type, value):
+    def __init__(self, type, value ,  line=None, column=None):
         # token type: INTEGER, PLUS, MINUS, MULTIPLY, DIVIDE, LPAREN, RPAREN, MODULO, EOF
         self.type = type
         # value: +, -, *, /, %, None
         self.value = value
+        self.line = line
+        self.column = column
 
     def __str__(self):
         """String representation of the class instance.
-
         Examples:
             Token(INTEGER, 3)
             Token(PLUS, '+')
         """
-        return 'Token({type}, {value})'.format(type=self.type, value=repr(self.value))
+        return 'Token({type}, {value} , position={line}:{column})'.format(
+            type=self.type,
+            value=repr(self.value),
+            line=self.line,
+            column=self.column)
 
     def __repr__(self):
         return self.__str__()
